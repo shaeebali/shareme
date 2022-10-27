73402c94e79bcff9946834495da20fd00bb4af9d
shaeeb <shaeebali@gmail.com>
2022-10-27 17:43:17 +0100

    auth credentials front end react

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+);
+
 
